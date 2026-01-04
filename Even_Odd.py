5351da6cc37cfad86ba918d160844611085403f6
Mohd Kashif <4kashifshaikh@gmail.com>
2026-01-04 11:41:24 +0530

    Error

--- Even_Odd.py
+++ Even_Odd.py
@@ -1,7 +1,8 @@
 #Even and Odd
 n=int(input("Enter the no"))
-if (n%2==0):
+if (n%2=0):
 	print("The number is Even",n)
 else:
-	print("The number is Odd",n)
+	print("The number is Odd",n))
+
 print("Completed")
